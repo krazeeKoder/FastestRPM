e23a21b6930e3b3d3a65965832b6e3a556a7b5b6
KrazeeKoder <tony.tulai@gmail.com>
2016-01-21 14:35:33 -0500

    Works and displays max Speed

--- FastestRPM/ViewController.m
+++ FastestRPM/ViewController.m
@@ -10,12 +10,56 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIImageView *needle;
+
+@property (assign) float needleSpeed;
+@property (assign) float maxSpeed;
+@property (assign) float needleLocation;
+@property (strong, nonatomic) IBOutlet UILabel *maxSpeedLabel;
+
+
 @end
 
 @implementation ViewController
+- (IBAction)panGestureRecongizer:(id)sender {
+
+    NSString *maxSpeedString = @"";
+    NSNumber *maxSpeedNumber = @(0);
+    CGFloat velocityX = [sender velocityInView:self.view].x;
+    
+    CGFloat velocityY = [sender velocityInView:self.view].y;
+    
+    self.needleSpeed = sqrt(velocityX*velocityX + velocityY*velocityY);
+    
+    if (self.needleSpeed > self.maxSpeed) {
+        self.maxSpeed = self.needleSpeed;
+    }
+    
+    if (self.needleSpeed > 6000.0) {
+        self.needleSpeed = 6000.0;
+    }
+    
+    self.needleLocation = self.needleSpeed / 21.58 + 137;
+
+        self.needle.transform = CGAffineTransformMakeRotation(self.needleLocation * M_PI/180);
+    
+    maxSpeedNumber = @(self.maxSpeed);
+    maxSpeedString = [maxSpeedNumber stringValue ];
+    self.maxSpeedLabel.text = [@"Max Speed (p/s): " stringByAppendingString:maxSpeedString];
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.maxSpeed = 3.0;
+    
+    float degrees = 137;
+    
+    self.needle.transform = CGAffineTransformMakeRotation(degrees * M_PI/180);
+    
+
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
